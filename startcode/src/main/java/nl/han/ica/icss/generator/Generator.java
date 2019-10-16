c5876cef54783ab6fa7f131622081a9c82f3b011
Demi van Kesteren <demi.van.kesteren@gmail.com>
2019-10-16 10:18:33 +0200

    Generator lvl0

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,10 +1,74 @@
 package nl.han.ica.icss.generator;
 
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.selectors.ClassSelector;
+import nl.han.ica.icss.ast.selectors.IdSelector;
+import nl.han.ica.icss.ast.selectors.TagSelector;
+
+import java.util.ArrayList;
 
 public class Generator {
 
+    private String result = "";
+
 	public String generate(AST ast) {
-        return "";
+	    generateResult(ast.root);
+        return result + "}";
 	}
+
+	private void generateResult(ASTNode node) {
+	    if(node instanceof Selector) {
+            if(result.endsWith(";\n")){
+                result += "}\n\n";
+            }
+	        generateSelectorResult((Selector) node);
+        } else if(node instanceof Declaration) {
+	        generateBodyResult(node);
+        }
+	    node.getChildren().forEach(this::generateResult);
+    }
+
+    private void generateSelectorResult(Selector node) {
+	    if(node instanceof ClassSelector) {
+	        result += ((ClassSelector) node).cls + " {";
+        } else if(node instanceof IdSelector) {
+	        result += ((IdSelector) node).id + " {";
+        } else if(node instanceof TagSelector) {
+	        result += ((TagSelector) node).tag + " {";
+        }
+	    result += "\n";
+    }
+
+    private void generateBodyResult(ASTNode node) {
+	    if(node instanceof Declaration) {
+            generateDeclarationResult(node.getChildren());
+        }
+    }
+
+    private void generateDeclarationResult(ArrayList<ASTNode> nodes) {
+	    for(ASTNode node : nodes) {
+            if (node instanceof PropertyName) {
+                result += "\t" + ((PropertyName) node).name + ": ";
+            }
+            if (node instanceof Expression) {
+                generateLiteralResult(node);
+            }
+        }
+        result += ";\n";
+    }
+
+    private void generateLiteralResult(ASTNode node) {
+	    if(node instanceof BoolLiteral) {
+	        result += ((BoolLiteral) node).value;
+        } else if(node instanceof ColorLiteral) {
+            result += ((ColorLiteral) node).value;
+        } else if(node instanceof PercentageLiteral) {
+	        result += ((PercentageLiteral) node).value + "%";
+        } else if(node instanceof PixelLiteral) {
+	        result += ((PixelLiteral) node).value + "px";
+        } else if(node instanceof ScalarLiteral) {
+	        result += ((ScalarLiteral) node).value;
+        }
+    }
 }
