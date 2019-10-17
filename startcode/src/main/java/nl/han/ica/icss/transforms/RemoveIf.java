8a829fc0e574c8c58075cb16af8f88f749136314
Demi van Kesteren <demi.van.kesteren@gmail.com>
2019-10-17 16:51:24 +0200

    It works, change to my code :p

--- startcode/src/main/java/nl/han/ica/icss/transforms/RemoveIf.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/RemoveIf.java
@@ -3,6 +3,7 @@ package nl.han.ica.icss.transforms;
 import nl.han.ica.icss.ast.*;
 import nl.han.ica.icss.ast.literals.BoolLiteral;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,33 +17,70 @@ public class RemoveIf implements Transform {
         variables = new LinkedList<>();
         variables.add(new HashMap<>());
 
-//        evaluateIfStatements(ast.root.getChildren(), ast.root);
+        evaluateIfStatements(ast.root.getChildren(), ast.root);
     }
 
     private void evaluateIfStatements(List<ASTNode> nodes, ASTNode parent) {
+//        for(ASTNode node : nodes) {
+//            if(node instanceof IfClause) {
+//                if(ifClause.conditionalExpression instanceof BoolLiteral) {
+//                    BoolLiteral boolLiteral = (BoolLiteral) ifClause.conditionalExpression;
+//                    //System.out.println(ifClause.conditionalExpression + " " + boolLiteral.value);
+//                    if(boolLiteral.value) {
+//                        //parent.removeChild(node);
+//                        for(ASTNode bodyNode : ifClause.body) {
+//                            parent.addChild(bodyNode);
+//                        }
+//                        evaluateIfStatements(ifClause.body, parent);
+//                    } else {
+//                        System.out.println(parent.getClass());
+//                        System.out.println("Before: \n" + parent.getChildren());
+//                        parent.removeChild(node);
+//                        System.out.println("To remove: \n" + node);
+//                        System.out.println("After remove: \n" + parent.getChildren());
+//                    }
+//                }
+//            }
+//            evaluateIfStatements(node.getChildren(), node);
+//        }
+
         for(ASTNode node : nodes) {
             if(node instanceof IfClause) {
-                IfClause ifClause = (IfClause) node;
+                evaluateIfClause((IfClause) node, parent);
+            }
+            if(node.getChildren() != null) {
+                evaluateIfStatements(node.getChildren(), node);
+            }
+        }
+    }
 
-                if(ifClause.conditionalExpression instanceof BoolLiteral) {
-                    BoolLiteral boolLiteral = (BoolLiteral) ifClause.conditionalExpression;
-                    //System.out.println(ifClause.conditionalExpression + " " + boolLiteral.value);
-                    if(boolLiteral.value) {
-                        //parent.removeChild(node);
-                        for(ASTNode bodyNode : ifClause.body) {
-                            parent.addChild(bodyNode);
-                        }
-                        evaluateIfStatements(ifClause.body, parent);
-                    } else {
-                        System.out.println(parent.getClass());
-                        System.out.println("Before: \n" + parent.getChildren());
-                        parent.removeChild(node);
-                        System.out.println("To remove: \n" + node);
-                        System.out.println("After remove: \n" + parent.getChildren());
-                    }
+    private List<ASTNode> evaluateIfClause(IfClause ifClause, ASTNode parent) {
+        List<ASTNode> values = new ArrayList<>();
+        if(((BoolLiteral) ifClause.conditionalExpression).value) {
+            for(ASTNode node : ifClause.body) {
+                if(node instanceof IfClause) {
+                    values.addAll(evaluateIfClause((IfClause) node, ifClause));
+                } else {
+                    values.add(node);
                 }
             }
-            evaluateIfStatements(node.getChildren(), node);
+        }
+        removeIfClause(ifClause, parent);
+        if(!(parent instanceof IfClause)) {
+            for(ASTNode node : values) {
+                parent.addChild(node);
+            }
+        }
+        return values;
+    }
+
+    private void removeIfClause(IfClause ifClause, ASTNode parent) {
+        if(parent instanceof Stylerule) {
+            Stylerule stylerule = (Stylerule) parent;
+            int index = stylerule.body.indexOf(ifClause);
+            stylerule.body.remove(index);
+        } else {
+            parent.removeChild(ifClause);
         }
     }
 }
