4e6e94c4d052535c706abdefa7ef0f4acc0ba709
Demi van Kesteren <demi.van.kesteren@gmail.com>
2019-10-19 22:07:39 +0200

    Added doc, created getBoolean and findAllVariables

--- startcode/src/main/java/nl/han/ica/icss/transforms/RemoveIf.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/RemoveIf.java
@@ -16,29 +16,46 @@ public class RemoveIf implements Transform {
     public void apply(AST ast) {
         variables = new LinkedList<>();
         variables.add(new HashMap<>());
+        findAllVariables(ast.root);
 
         evaluateIfStatements(ast.root.getChildren(), ast.root);
     }
 
-    private void evaluateIfStatements(List<ASTNode> nodes, ASTNode parent) {
-        for(ASTNode node : nodes) {
-            if(node instanceof IfClause) {
-                evaluateIfClause((IfClause) node, parent);
+    /**
+     * For every child, if the child is an IfClause, evaluate the child
+     *
+     * @param children children of the parent
+     * @param parent node to evaluate
+     */
+    private void evaluateIfStatements(List<ASTNode> children, ASTNode parent) {
+        for(ASTNode child : children) {
+            if(child instanceof IfClause) {
+                evaluateIfClause((IfClause) child, parent);
             }
-            if(node.getChildren() != null) {
-                evaluateIfStatements(node.getChildren(), node);
-            }
+            evaluateIfStatements(child.getChildren(), child);
         }
     }
 
+    /**
+     * Checks if the condition is true or false.
+     * If true, keep the body,
+     * If false, remove it.
+     *
+     * @param ifClause ifClause to evaluate
+     * @param parent parent of the ifClause
+     * @return list of node to keep
+     */
     private List<ASTNode> evaluateIfClause(IfClause ifClause, ASTNode parent) {
         List<ASTNode> values = new ArrayList<>();
-        if(((BoolLiteral) ifClause.conditionalExpression).value) {
-            for(ASTNode node : ifClause.body) {
-                if(node instanceof IfClause) {
-                    values.addAll(evaluateIfClause((IfClause) node, ifClause));
-                } else {
-                    values.add(node);
+
+        if(ifClause.conditionalExpression instanceof BoolLiteral || ifClause.conditionalExpression instanceof VariableReference) {
+            if (getBoolean(ifClause.conditionalExpression).value) {
+                for (ASTNode node : ifClause.body) {
+                    if (node instanceof IfClause) {
+                        values.addAll(evaluateIfClause((IfClause) node, ifClause));
+                    } else {
+                        values.add(node);
+                    }
                 }
             }
         }
@@ -51,6 +68,32 @@ public class RemoveIf implements Transform {
         return values;
     }
 
+    /**
+     * Checks if the node is a BoolLiteral or VariableReference
+     * and returns the value (true or false) of the node.
+     *
+     * @param node node to get the value from
+     * @return value of the node or null
+     */
+    private BoolLiteral getBoolean(ASTNode node) {
+        if(node instanceof BoolLiteral) {
+            return (BoolLiteral) node;
+        }
+        if(node instanceof VariableReference) {
+            VariableReference variableReference = (VariableReference) node;
+            if(variables.getFirst().containsKey(variableReference.name)) {
+                return (BoolLiteral) variables.getFirst().get(variableReference.name);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Removes the ifClause from the parent.
+     *
+     * @param ifClause ifClause to remove
+     * @param parent parent of the IfClause
+     */
     private void removeIfClause(IfClause ifClause, ASTNode parent) {
         if(parent instanceof Stylerule) {
             Stylerule stylerule = (Stylerule) parent;
@@ -59,5 +102,19 @@ public class RemoveIf implements Transform {
         } else {
             parent.removeChild(ifClause);
         }
+    }
+
+    /**
+     * Adds the variable name and expression to the hashmap in variables
+     *
+     * @param toBeFound VariableAssignment to find
+     */
+    private void findAllVariables(ASTNode toBeFound) {
+        if(toBeFound instanceof VariableAssignment) {
+            String name = ((VariableAssignment) toBeFound).name.name;
+            Expression expression = ((VariableAssignment) toBeFound).expression;
+            variables.getFirst().put(name, expression);
+        }
+        toBeFound.getChildren().forEach(this::findAllVariables);
     }
 }
